544020fdd530ec2d2fa2de883f461316ef674f1b
humbleone66 <107198825+humbleone66@users.noreply.github.com>
2025-11-15 06:34:19 -0400

    Add files via upload

---
+++ PraiseGod/Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+
+    namespace Great
+    {
+        class Job
+        {
+            static void Main(string[] args)
+            {
+
+    Console.WriteLine("What was the first thing God said in Genesis?");
+    string answer = Console.ReadLine();
+    Console.WriteLine(answer); 
+    Console.WriteLine("Let there be light");
+
+    Console.WriteLine("What did God make the two great lights for?");
+    answer = Console.ReadLine();
+    Console.WriteLine(answer); 
+    Console.WriteLine("One to rule the day and one to rule the night");
+
+    Console.WriteLine("What did God call the dry land?");
+    answer = Console.ReadLine();
+    Console.WriteLine(answer); 
+    Console.WriteLine("Earth");
+
+    Console.WriteLine("What was the name of the first man?");
+    answer = Console.ReadLine();
+    Console.WriteLine(answer); 
+    Console.WriteLine("Adam");
+
+    Console.WriteLine("What was the name of the first woman?");
+    answer = Console.ReadLine();
+    Console.WriteLine(answer); 
+    Console.WriteLine("Eve");
+
+    Console.WriteLine("What was the name of Adam and Eve's two first children?");
+    answer = Console.ReadLine();
+    Console.WriteLine(answer); 
+    Console.WriteLine("Cain and Abel");
+
+    Console.WriteLine("When did God rest from doing his work?");
+    answer = Console.ReadLine();
+    Console.WriteLine(answer); 
+    Console.WriteLine("On the seventh day");
+
+    Console.WriteLine("In Genesis 2, what did God do to the 7th day?");
+    answer = Console.ReadLine();
+    Console.WriteLine(answer); 
+    Console.WriteLine("He blessed it and sanctified it");
+
+    Console.WriteLine("What was man formed from in Genesis 2?");
+    answer = Console.ReadLine();
+    Console.WriteLine(answer); 
+    Console.WriteLine("The dust of the ground");
+
+    Console.WriteLine("After God formed man from dust, what did he do to give him life?");
+    answer = Console.ReadLine();
+    Console.WriteLine(answer); 
+    Console.WriteLine("He breathed into his nostrils the breath of life");
+
+    Console.WriteLine("Where did God plant the Garden in Eden?");
+    answer = Console.ReadLine();
+    Console.WriteLine(answer); 
+    Console.WriteLine("Eastward");
+
+    Console.WriteLine("Where did God put man when he formed him?");
+    answer = Console.ReadLine();
+    Console.WriteLine(answer); 
+    Console.WriteLine("Eastward, in the Garden of Eden?");
+
+    Console.WriteLine("What two trees did God put in the midst of the Garden?");
+    answer = Console.ReadLine();
+    Console.WriteLine(answer); 
+    Console.WriteLine("The tree of life and the tree of good and evil");
+
+    Console.WriteLine("What was the name of the first river in Genesis 2?");
+    answer = Console.ReadLine();
+    Console.WriteLine(answer); 
+    Console.WriteLine("Pison");
+
+    Console.WriteLine("What does this river compass?");
+    answer = Console.ReadLine();
+    Console.WriteLine(answer); 
+    Console.WriteLine("Havilah");
+
+        Console.WriteLine("What minerals are in Havilah?");
+    answer = Console.ReadLine();
+    Console.WriteLine(answer); 
+    Console.WriteLine("Gold, bdellium and the onyx stone");
+
+        Console.WriteLine("What is the name of the second river?");
+    answer = Console.ReadLine();
+    Console.WriteLine(answer); 
+    Console.WriteLine("Gihon");
+
+        Console.WriteLine("What country does Gihon compass?");
+    answer = Console.ReadLine();
+    Console.WriteLine(answer); 
+    Console.WriteLine("Ethiopia");
+
+        Console.WriteLine("What is the name of the third river?");
+    answer = Console.ReadLine();
+    Console.WriteLine(answer); 
+    Console.WriteLine("Hiddekel");
+
+        Console.WriteLine("Where is it?");
+    answer = Console.ReadLine();
+    Console.WriteLine(answer); 
+    Console.WriteLine("It goes towards the east of Assyria");
+
+        Console.WriteLine("What is the fourth river?");
+    answer = Console.ReadLine();
+    Console.WriteLine(answer); 
+    Console.WriteLine("Euphrates");
+
+        Console.WriteLine("What tree was Adam and Eve forbidden to eat from?");
+    answer = Console.ReadLine();
+    Console.WriteLine(answer); 
+    Console.WriteLine("The Tree Of Good and Evil");
+
+        Console.WriteLine("How were the beasts of the field and fowls of the air named?");
+    answer = Console.ReadLine();
+    Console.WriteLine(answer); 
+    Console.WriteLine("God brought them to Adam to see what he would name them, and so they were named");
+
+        Console.WriteLine("How was Eve created?");
+    answer = Console.ReadLine();
+    Console.WriteLine(answer); 
+    Console.WriteLine("God did not want man to be alone, he wanted him to have a helper, so he caused Adam to fall into a deep sleep, then he made Eve from one of Adam's ribs, then she was brought to Adam");
+
+        Console.WriteLine("What did Adam say when Eve was created?");
+    answer = Console.ReadLine();
+    Console.WriteLine(answer); 
+    Console.WriteLine("This is now bone of my bones, and flesh of my flesh; she shall be called Woman, because she was taken out of Man");
+
+            }
+        }
+    }
